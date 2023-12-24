bbb97782144ed98ed7cfed6b934ef9b234c092db
Potassium <brainiacgamer444@gmail.com>
2023-12-25 01:48:55 +0545

    Create educational information section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import General from "./components/General";
+import Educational from "./components/Educational";
 
 export default function App() {
   return (
-    <div className="flex justify-center min-h-screen min-w-full bg-[#171717]">
+    <div className="flex flex-col min-h-screen min-w-full bg-[#171717]">
       <General />
+      <Educational />
     </div>
   );
 }
--- src/components/Educational.jsx
+++ src/components/Educational.jsx
@@ -1,32 +1,28 @@
-export default function General() {
+export default function Educational() {
   return (
-    <div className="text-white flex flex-col my-[50px] mx-[100px] gap-[30px]">
-      <h2 className="pl-[20px] text-3xl font-bold">General Information</h2>
+    <div className="flex flex-col text-white my-[50px] mx-[100px] gap-[30px]">
+      <h2 className="text-white text-3xl font-bold pl-[20px]">Educational Experience</h2>
       <div className=" bg-[#202020] p-[40px] rounded-lg gap-[20px] min-w-[100%]">
         <form className="grid grid-cols-2 gap-[50px]">
           <div className="flex flex-col gap-[10px]">
-            <label className="font-semibold">Full Name</label>
-            <input
-              type="text"
-              placeholder="Spongebob Squarepants"
-              className="border-2 bg-[#474747] text-white h-[40px] p-[7px] placeholder:text-[#c0c0c0]"
-            ></input>
-          </div>
-          <div className="flex flex-col gap-[10px]">
-            <label className="font-semibold">Email</label>
-            <input
-              type="email"
-              placeholder="spongebob123@gmail.com"
-              className="border-2 bg-[#474747] text-white h-[40px] p-[7px] placeholder:text-[#c0c0c0]"
-            ></input>
-          </div>
-          <div className="flex flex-col gap-[10px]">
             <label className="font-semibold">
-              Phone Number <small>(Optional)</small>
+              School Name <small>(Short form works)</small>
             </label>
             <input
-              type="number"
-              placeholder="+123-1234567890"
+              type="text"
+              placeholder="Name of the school"
+              className="border-2 bg-[#474747] text-white h-[40px] p-[7px] placeholder:text-[#c0c0c0]"
+            ></input>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label className="font-semibold">Title of Study</label>
+            <input type="text" className="border-2 bg-[#474747] text-white h-[40px] p-[7px] placeholder:text-[#c0c0c0]"></input>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label className="font-semibold">Date of Study</label>
+            <input
+              type="text"
+              placeholder="From-To (Year)"
               className="border-2 bg-[#474747] text-white h-[40px] p-[7px] placeholder:text-[#c0c0c0]"
             ></input>
           </div>
@@ -36,7 +32,7 @@ export default function General() {
             </label>
             <input
               type="text"
-              placeholder="City name"
+              placeholder="School Address"
               className="border-2 bg-[#474747] text-white h-[40px] p-[7px] placeholder:text-[#c0c0c0]"
             ></input>
           </div>
