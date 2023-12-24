e23c2463c341718c296a7197a569b843a519ab2b
Potassium <brainiacgamer444@gmail.com>
2023-12-25 02:05:17 +0545

    Create practical information section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import General from "./components/General";
 import Educational from "./components/Educational";
+import Practical from "./components/Practical";
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-[#171717]">
       <General />
       <Educational />
+      <Practical />
     </div>
   );
 }
---
+++ src/components/Practical.jsx
@@ -0,0 +1,45 @@
+export default function Practical() {
+  return (
+    <div className="flex flex-col text-white my-[50px] mx-[100px] gap-[30px]">
+      <h2 className="text-white text-3xl font-bold pl-[20px]">Practical Experience</h2>
+      <div className=" bg-[#202020] p-[40px] rounded-lg gap-[20px] min-w-[100%]">
+        <form className="grid grid-cols-2 gap-[50px]">
+          <div className="flex flex-col gap-[10px]">
+            <label className="font-semibold">
+              Company Name <small>(Short form works)</small>
+            </label>
+            <input
+              type="text"
+              placeholder="Name of the company"
+              className="border-2 bg-[#474747] text-white h-[40px] p-[7px] placeholder:text-[#c0c0c0]"
+            ></input>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label className="font-semibold">Position Title</label>
+            <input
+              type="text"
+              placeholder="Title of the job"
+              className="border-2 bg-[#474747] text-white h-[40px] p-[7px] placeholder:text-[#c0c0c0]"
+            ></input>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label className="font-semibold">Main Responsibilty of your jobs</label>
+            <textarea
+              type=""
+              placeholder="Describe your job role in brief"
+              className="border-2 bg-[#474747] text-white min-h-[150px] p-[7px] placeholder:text-[#c0c0c0]"
+            ></textarea>
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label className="font-semibold">Date From and Until</label>
+            <input
+              type="text"
+              placeholder="2010 - 2018"
+              className="border-2 bg-[#474747] text-white h-[40px] p-[7px] placeholder:text-[#c0c0c0]"
+            ></input>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
